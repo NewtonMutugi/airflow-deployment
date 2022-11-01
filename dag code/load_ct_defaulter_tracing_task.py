132a3a4b86295ac5c4db737bbac1e9645e9ad65e
Paul Nthusi <paul.nthusi@thepalladiumgroup.com>
2022-11-01 13:54:13 +0300

    Add additional task files

--- dag code/load_ct_defaulter_tracing_task.py
+++ dag code/load_ct_defaulter_tracing_task.py
@@ -26,50 +26,42 @@ dc_spark_source_driver = Variable.get("DC_SPARK_SOURCE_DRIVER")
 dc_spark_source_user = Variable.get("DC_SPARK_SOURCE_USER")
 dc_spark_source_password = Variable.get("DC_SPARK_SOURCE_PASSWORD")
 dc_spark_source_numpartitions = Variable.get("DC_SPARK_SOURCE_NUMPARTITIONS")
-dc_spark_sink_numpartitions = Variable.get("DC_SPARK_SINK_NUMPARTITIONS")
 dc_spark_sink_url = Variable.get("DC_SPARK_SINK_URL")
 dc_spark_sink_driver = Variable.get("DC_SPARK_SINK_DRIVER")
 dc_spark_sink_user = Variable.get("DC_SPARK_SINK_USER")
 dc_spark_sink_password = Variable.get("DC_SPARK_SINK_PASSWORD")
 
-ctpv_spark_source_metadata_table = Variable.get("CTPV_SPARK_SOURCE_METADATA_TABLE")
-ctpv_spark_sink_dbtable = Variable.get("CTPV_SPARK_SINK_DBTABLE")
 
-def build_load_ct_patient_visits_task(dag:DAG):
-    load_ct_patient_visits = SparkSubmitOperator(task_id='load_ct_patient_visits',
+lctdt_spark_sink_dbtable = Variable.get("LCTDT_SPARK_SINK_DBTABLE")
+
+def build_load_ct_defaulter_tracing_task(dag: DAG) -> SparkSubmitOperator:
+    load_ct_defaulter_tracing = SparkSubmitOperator(task_id='load_ct_defaulter_tracing',
                                               conn_id='spark_standalone',
-                                              application=f'{spark_app_home}/load-ct-patient-visits-1.0-SNAPSHOT-jar-with-dependencies.jar',
+                                              application=f'{spark_app_home}/load-ct-defaulter-tracing-1.0-SNAPSHOT-jar-with-dependencies.jar',
                                               total_executor_cores = default_spark_total_executor_cores,
                                               executor_cores = default_spark_executor_cores,
                                               executor_memory = default_spark_executor_memory,
                                               driver_memory = default_spark_driver_memory,
-                                              name='load_ct_patient_visits',
-                                              conf={
-                                                # "spark.openlineage.namespace": spark_openlineage_namespace,
-                                                # "spark.openlineage.host": spark_openlineage_host,
-                                                # "spark.extraListeners": spark_extra_listeners,                                                
+                                              name='load_ct_defaulter_tracing',
+                                              conf={ 
                                                 "spark.driver.port":spark_driver_port,
                                                 "spark.driver.blockManager.port":spark_driver_block_manager_port,
                                                 "spark.driver.host": spark_driver_host,
                                                 "spark.driver.bindAddress": spark_driver_bind_address,
                                                 "spark.source.database-name": dc_spark_source_database_name,
-                                                "spark.source.metadata-table": ctpv_spark_source_metadata_table,
                                                 "spark.source.database-host": dc_spark_source_database_host,
                                                 "spark.source.url": dc_spark_source_url,
                                                 "spark.source.driver": dc_spark_source_driver,
                                                 "spark.source.user": dc_spark_source_user,
                                                 "spark.source.password": dc_spark_source_password,
                                                 "spark.source.numpartitions": dc_spark_source_numpartitions,
-                                                "spark.sink.numpartitions":dc_spark_sink_numpartitions,
                                                 "spark.sink.url": dc_spark_sink_url,
                                                 "spark.sink.driver": dc_spark_sink_driver,
                                                 "spark.sink.user":dc_spark_sink_user ,
                                                 "spark.sink.password":dc_spark_sink_password,
-                                                "spark.sink.dbtable":ctpv_spark_sink_dbtable,
-                                                "spark.sql.autoBroadcastJoinThreshold":-1,
-                                                "spark.sink.partitions": 10                              
+                                                "spark.sink.dbtable":lctdt_spark_sink_dbtable
                                               },
-                                              execution_timeout=timedelta(minutes=600),
+                                              execution_timeout=timedelta(minutes=10),
                                               dag=dag
                                               )
-    return load_ct_patient_visits
+    return load_ct_defaulter_tracing
