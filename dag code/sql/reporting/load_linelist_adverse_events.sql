b421486278a70f250945a0340d0dd4e013d87060
cbrianbet <cbrianbet@outlook.com>
2023-10-18 19:26:28 +0300

    sql scripts for reporting added

---
+++ dag code/sql/reporting/load_linelist_adverse_events.sql
@@ -0,0 +1,39 @@
+IF OBJECT_ID(N'[REPORTING].[dbo].LineListAdverseEvents', N'U') IS NOT NULL 		
+	drop TABLE [REPORTING].[dbo].LineListAdverseEvents;
+
+with AdverseEvents as (
+ SELECT
+            MFLCode,
+            PatientIDHash,
+            PatientPKHash,
+            NUPI,
+            g.DATIMAgeGroup,
+            pat.Gender,
+            f.FacilityName,
+            County,
+            SubCounty,
+            p.PartnerName,
+            a.AgencyName,
+            AdverseEvent,
+            AdverseEventCause,
+            AdverseEventRegimen,
+            AdverseEventActionTaken,
+            Severity,
+            CAST(GETDATE() AS DATE) AS LoadDate 
+        FROM
+            [NDWH].dbo.FactAdverseEvents it
+            INNER join NDWH.dbo.DimFacility f on f.FacilityKey = it.FacilityKey
+            INNER JOIN NDWH.dbo.DimAgency a on a.AgencyKey = it.AgencyKey
+            INNER JOIN NDWH.dbo.DimPatient pat on pat.PatientKey = it.PatientKey
+            INNER JOIN NDWH.dbo.DimPartner p on p.PartnerKey = it.PartnerKey
+            INNER JOIN NDWH.dbo.FactART art on art.PatientKey = it.PatientKey
+            LEFT join NDWH.dbo.DimAgeGroup g on g.Age = art.AgeLastVisit
+        WHERE
+            pat.IsTXCurr = 1
+)
+
+SELECT
+   *
+ INTO [REPORTING].[dbo].LineListAdverseEvents
+FROM AdverseEvents
+GO
