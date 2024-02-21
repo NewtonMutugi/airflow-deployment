bb7326e0165a081e42dab03cb33a004295ca9fc3
cbrianbet <cbrianbet@outlook.com>
2024-02-21 14:50:17 +0300

    Added missing dags
    added ldl durable dag
    added concordance txcurr

---
+++ production/plugins/includes/reporting/load_aggregate_concordance_TxCurr_task.py
@@ -0,0 +1,13 @@
+from airflow import DAG
+from datetime import timedelta
+from airflow.providers.microsoft.mssql.operators.mssql import MsSqlOperator
+
+
+def build_load_aggregate_concordance_TxCurr_task(dag: DAG):
+    load_aggregate_concordance_TxCurr_task = MsSqlOperator(task_id='load_aggregate_concordance_TxCurr_task',
+                                              mssql_conn_id='reporting',
+                                              execution_timeout=timedelta(minutes=600),
+                                              sql='sql/reporting/load_aggregate_concordance_TxCurr.sql',
+                                              dag=dag
+                                              )
+    return load_aggregate_concordance_TxCurr_task
