ce1d7b48c47b80cc9faaebb413d7e87dda0c1026
Paul Nthusi <paul.nthusi@thepalladiumgroup.com>
2024-02-22 15:46:16 +0300

    Bug fix

--- production/plugins/includes/load_intermediate_pbfw_task.py
+++ production/plugins/includes/load_intermediate_pbfw_task.py
@@ -1,20 +1,17 @@
 from airflow import DAG
 from datetime import datetime, timedelta
-from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
+from airflow.operators.bash_operator import BashOperator
 
 def build_load_intermediate_pbfw_task(dag: DAG, default_conf):
-    intermediate_pbfw = SparkSubmitOperator(task_id='intermediate_pbfw',
-                                                   conn_id=default_conf['connection_id'],
-                                                   application=f"{default_conf['spark_app_home']}/load-intermediate-pbfw-1.0-SNAPSHOT-jar-with-dependencies.jar",
-                                                   total_executor_cores=default_conf[
-                                                       'default_spark_total_executor_cores'],
-                                                   executor_cores=default_conf['default_spark_executor_cores'],
-                                                   executor_memory=default_conf['default_spark_executor_memory'],
-                                                   driver_memory=default_conf['default_spark_driver_memory'],
-                                                   name='intermediate_pbfw',
-                                                   conf=default_conf,
-                                                   execution_timeout=timedelta(
-                                                       minutes=600),
-                                                   dag=dag
-                                                   )
+    intermediate_pbfw = BashOperator(task_id='intermediate_pbfw',
+                          dag=dag,
+                          bash_command=f"java -jar {default_conf['spark_app_home']}/load-intermediate-pbfw-1.0-SNAPSHOT-jar-with-dependencies.jar",
+                          env={
+                              "ODS_HOST_URL": default_conf['spark.ods.url'],
+                              "ODS_USER": default_conf['spark.ods.user'],
+                              "ODS_PASSWORD": default_conf['spark.ods.password'],
+                              "ODS_QUERY_TIMEOUT": default_conf['spark.intermediateQuery.timeout']
+                          },
+                          execution_timeout=timedelta(minutes=600)
+                          )
     return intermediate_pbfw
