f0b75028a387fdaaf8a9da25c8d157457f318c8e
cbrianbet <cbrianbet@outlook.com>
2024-02-28 11:26:43 +0300

    superset trigger dag and tasks added

---
+++ production/dags/superset_trigger_dag.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+import pendulum
+from includes.superset.superset_dataset_description_trigger_task import build_superset_dataset_description_trigger_task
+from includes.superset.superset_dataset_refresh_trigger_task import build_superset_dataset_refresh_trigger_task
+
+
+local_tz = pendulum.timezone("Africa/Nairobi")
+default_args = {
+    'owner': 'kenyahmis',
+    'depends_on_past': False,
+    'start_date': datetime(2022, 10, 13, tzinfo=local_tz),
+    'email': ['paul.nthusi@thepalladiumgroup.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'superset_background_jobs_trigger_dag',
+    default_args=default_args,
+    description='Superset background jobs trigger dag',
+    schedule_interval='0 4 * * *',
+)
+
+dataset_description = build_superset_dataset_description_trigger_task.superset_dataset_description_trigger
+dataset_refresh = build_superset_dataset_refresh_trigger_task.superset_dataset_refresh_trigger
+
+
+dataset_description >> dataset_refresh
