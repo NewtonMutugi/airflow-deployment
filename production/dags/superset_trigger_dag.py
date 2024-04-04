feeefe49114e1a87f41a55799ac87f7dd2167732
Paul Nthusi <paul.nthusi@thepalladiumgroup.com>
2024-04-04 19:02:15 +0300

    Update task frequency

--- production/dags/superset_trigger_dag.py
+++ production/dags/superset_trigger_dag.py
@@ -10,7 +10,7 @@ local_tz = pendulum.timezone("Africa/Nairobi")
 default_args = {
     'owner': 'kenyahmis',
     'depends_on_past': False,
-    'start_date': datetime(2022, 10, 13, tzinfo=local_tz),
+    'start_date': datetime(2024, 4, 4, tzinfo=local_tz),
     'email': ['paul.nthusi@thepalladiumgroup.com'],
     'email_on_failure': True,
     'email_on_retry': True,
@@ -22,7 +22,8 @@ dag = DAG(
     'superset_background_jobs_trigger_dag',
     default_args=default_args,
     description='Superset background jobs trigger dag',
-    schedule_interval='0 4 * * *',
+    catchup=False,
+    schedule_interval='0 2 * * 1,3',
 )
 dataset_create = build_superset_dataset_create_trigger_task(dag = dag)
 dataset_description = build_superset_dataset_description_trigger_task(dag = dag)
